f4c2beb693939adfbfdb941b8311fff858f2e39c
Fernando Pereira <fernando@amojee.com>
2017-01-03 16:07:03 -0800

    Initial Commit

---
+++ TRZPhotoPickerTray/TRZPhotoPickerTray.h
@@ -0,0 +1,19 @@
+//
+//  TRZPhotoPickerTray.h
+//  TRZPhotoPickerTray
+//
+//  Created by Fernando Pereira on 1/3/17.
+//  Copyright © 2017 Troezen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRZPhotoPickerTray.
+FOUNDATION_EXPORT double TRZPhotoPickerTrayVersionNumber;
+
+//! Project version string for TRZPhotoPickerTray.
+FOUNDATION_EXPORT const unsigned char TRZPhotoPickerTrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRZPhotoPickerTray/PublicHeader.h>
+
+
